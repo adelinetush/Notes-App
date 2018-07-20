9ac418b7b244d976531405ffe71c48f9b4f517d3
Ad <adelinetush@gmail.com>
2018-07-20 12:52:35 +0300

    some changes

---
+++ run.py
@@ -0,0 +1,12 @@
+#!flask/bin/python
+# 
+import os 
+from app import app
+
+#app.run(debug=True)
+port = int(os.environ.get('PORT',5000))
+if(__name__=="__main__"):
+    app.config['DEBUG'] = True
+
+    app.debug = True
+    app.run(host='0.0.0.0',port=port)
