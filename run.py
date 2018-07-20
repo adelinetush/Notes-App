3aad2ee34fe9f812a8de2779c66f151f5a1aa73a
Ad <adelinetush@gmail.com>
2018-07-20 16:17:17 +0300

    edit port again

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import os
 from app import app
 
 #app.run(debug=True)
-port = int(os.environ.get('PORT',80))
+port = int(os.environ.get('PORT',8080))
 if(__name__=="__main__"):
     app.config['DEBUG'] = True
 
