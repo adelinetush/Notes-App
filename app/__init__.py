b0c6a89d1c2a48db5643f8e197452cbf1bc65a2f
Ad <adelinetush@gmail.com>
2018-07-20 13:55:53 +0300

    updated db details

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,14 @@ from app.models import db
 app = Flask(__name__)
 
 POSTGRES = {
+    'user': 'ujunsnkoqsoxle',
+    'pw': 'dc931a20cb777d5baa5060c4061b608ae41f51564112e9616d5b845662cea0d7',
+    'db': 'deve9bdv2opfal',
+    'host': 'ec2-23-21-216-174.compute-1.amazonaws.com',
+    'port': '5432',
+}
+
+POSTGRESTEST = {
     'user': 'postgres',
     'pw': 'root',
     'db': 'diary',
@@ -12,9 +20,9 @@ POSTGRES = {
 }
 
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/diary'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
-#%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/diary'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
 
 db.init_app(app)
 
